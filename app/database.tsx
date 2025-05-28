13c36d085f169d0108e03baf54a02750615ad07f
JohnDoli <JohnDoli@users.noreply.github.com>
2025-05-28 11:39:04 +0200

    try/connecting to db

---
+++ app/database.tsx
@@ -0,0 +1,19 @@
+// import * as SQLite from 'expo-sqlite';
+
+// const db = await SQLite.openDatabaseAsync('stopwatch.db');
+
+// // await db.execAsync(`
+// //     CREATE TABLE IF NOT EXISTS test (id INTEGER PRIMARY KEY NOT NULL, value TEXT NOT NULL, intValue INTEGER);
+// //     INSERT INTO test (value, intValue) VALUES ('test1', 123);
+// //     INSERT INTO test (value, intValue) VALUES ('test2', 456);
+// //     INSERT INTO test (value, intValue) VALUES ('test3', 789);
+// // `);
+
+// interface TimeItem {
+// 	id: number;
+// 	value: string;
+// 	intValue: number;
+// }
+
+// const firstRow = await db.getFirstAsync('SELECT * FROM timeitems') as TimeItem;
+// console.log(firstRow.id, firstRow.value, firstRow.intValue);
--- app/index.tsx
+++ app/index.tsx
@@ -3,44 +3,61 @@ import { Text, View, StyleSheet, Pressable, ScrollView } from "react-native";
 import TimeItem from './components/timeItem';
 import { Stack } from 'expo-router';
 import React, { useState } from 'react';
+import * as SQLite from 'expo-sqlite';
 
 
 interface HeaderProps {
   onAddItem(): void;
+  onFirstRow(): void;
 }
 
-function HeaderIndex({ onAddItem }: HeaderProps) {
+function HeaderIndex({ onAddItem, onFirstRow: onCreateDB }: HeaderProps) {
   return (
     <View style={stylesHeader.header}>
-      <Pressable onPress={() => console.log('Menu clicked')}>
-        <Ionicons name="airplane" size={24} color="black" />
+      <Pressable onPress={() => {console.log('Menu clicked'); onCreateDB();}}>
+        {/* <Ionicons name="airplane" size={24} color="black" /> */}
+        <Ionicons name="menu" size={24} color="black" />
       </Pressable>
 
       <Text style={stylesHeader.heading}>your time</Text>
 
       <Pressable onPress={() => {console.log('Plus clicked'); onAddItem();}}>
-        <Ionicons name="home" size={24} color="black" />
-        <Ionicons name="home" size={24} color="black" />
+        {/* <Ionicons name="home" size={24} color="black" />
+        <Ionicons name="home" size={24} color="black" /> */}
+        <Ionicons name="add" size={24} color="black" />
       </Pressable>
     </View>
   );
 }
 
+
 export default function Index() {
   
   const [items, setItems] = useState([{ text: '00:00:00' }]);
-
-  
 
   const addItem = () => {
     setItems(prevItems => [...prevItems, { text: '00:00:00' }]);
   };
 
+  const firstRow = async () => {
+    const db = await SQLite.openDatabaseAsync('stopwatch.db');
+
+    await db.execAsync(`
+      CREATE TABLE IF NOT EXISTS test (id INTEGER PRIMARY KEY NOT NULL, value TEXT NOT NULL, intValue INTEGER);
+      INSERT INTO test (value, intValue) VALUES ('test1', 123);
+      INSERT INTO test (value, intValue) VALUES ('test2', 456);
+      INSERT INTO test (value, intValue) VALUES ('test3', 789);
+    `);
+
+    const result = await db.execAsync(`SELECT * FROM test LIMIT 1;`);
+    console.log(result);
+  }
+
   return (
     <View style={styles.container}>
       <Stack.Screen
         options={{
-          header: () => <HeaderIndex onAddItem={addItem} />,
+          header: () => <HeaderIndex onAddItem={addItem} onFirstRow={firstRow} />,
         }}
       />
 
