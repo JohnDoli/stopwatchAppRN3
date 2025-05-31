8b876bcab14bd4fedd9e2e4dc8a8e58d78b56222
JohnDoli <john.dolik@gmail.com>
2025-05-31 21:48:04 +0200

    removed wrong db imports

--- app/index.tsx
+++ app/index.tsx
@@ -8,10 +8,9 @@ import * as SQLite from 'expo-sqlite';
 
 interface HeaderProps {
   onAddItem(): void;
-  onFirstRow(): void;
 }
 
-function HeaderIndex({ onAddItem, onFirstRow: onCreateDB }: HeaderProps) {
+function HeaderIndex({ onAddItem }: HeaderProps) {
   return (
     <View style={stylesHeader.header}>
       <Pressable onPress={() => console.log('Menu clicked')}>
@@ -37,25 +36,11 @@ export default function Index() {
     setItems(prevItems => [...prevItems, { shownTime: '00:00:00' }]);
   };
 
-  const firstRow = async () => {
-    const db = await SQLite.openDatabaseAsync('stopwatch.db');
-
-    await db.execAsync(`
-      CREATE TABLE IF NOT EXISTS test (id INTEGER PRIMARY KEY NOT NULL, value TEXT NOT NULL, intValue INTEGER);
-      INSERT INTO test (value, intValue) VALUES ('test1', 123);
-      INSERT INTO test (value, intValue) VALUES ('test2', 456);
-      INSERT INTO test (value, intValue) VALUES ('test3', 789);
-    `);
-
-    const result = await db.execAsync(`SELECT * FROM test LIMIT 1;`);
-    console.log(result);
-  }
-
   return (
     <View style={styles.container}>
       <Stack.Screen
         options={{
-          header: () => <HeaderIndex onAddItem={addItem} onFirstRow={firstRow} />,
+          header: () => <HeaderIndex onAddItem={addItem} />,
         }}
       />
       <ScrollView style={styles.itemsWrapper}>
