7f0056e5684395257a0777d3bf14404fa1fe04b0
JohnDoli <JohnDoli@users.noreply.github.com>
2025-05-31 12:27:09 +0200

    create table if not exist

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,10 +1,24 @@
 import { Stack } from "expo-router";
+import { SQLiteDatabase, SQLiteProvider } from "expo-sqlite";
 
 export default function RootLayout() {
+
+  const createDbIfNeeded = async (db: SQLiteDatabase) => {
+    console.log("Creating database if needed...");
+    await db.execAsync(`
+    CREATE TABLE IF NOT EXISTS test (id INTEGER PRIMARY KEY NOT NULL, value TEXT NOT NULL, intValue INTEGER);
+    INSERT INTO test (value, intValue) VALUES ('test1', 123);
+    INSERT INTO test (value, intValue) VALUES ('test2', 456);
+    INSERT INTO test (value, intValue) VALUES ('test3', 789);
+    `);
+  }
+
   return(
-    <Stack>
-      <Stack.Screen name="index" />
-      <Stack.Screen name="stopwatchScreen" />
-    </Stack>
+    <SQLiteProvider databaseName="stopwatch.db" onInit={createDbIfNeeded}>
+      <Stack>
+        <Stack.Screen name="index" />
+        <Stack.Screen name="stopwatchScreen" />
+      </Stack>
+    </SQLiteProvider>
   );
 }
